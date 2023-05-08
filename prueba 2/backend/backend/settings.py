5fedabf394cf913821ea2b06e03a7cb97988bfc1
JMKademian <96548645+JMKademian@users.noreply.github.com>
2023-05-08 19:18:29 -0300

    Update settings.py

--- prueba 2/backend/backend/settings.py
+++ prueba 2/backend/backend/settings.py
@@ -16,6 +16,7 @@ import warnings
 # Build paths inside the project like this: os.path.join(BASE_DIR, ...)
 BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 
+#SECRET_KEY = 'asfsdgfgerhet'
 
 # Quick-start development settings - unsuitable for production
 # See https://docs.djangoproject.com/en/2.1/howto/deployment/checklist/
@@ -239,3 +240,4 @@ WEBPACK_LOADER = {
 
 if env("ENVIRONMENT", "local") != 'local':
     COMPRESS_OFFLINE = True
+
